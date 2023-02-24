2717ea28215c11d43a5b8c33aac426eedf61365c
Errormacr <tulupov.valeriy@mail.ru>
2023-02-24 20:45:55 +0300

    Сделал аунтификацию

--- server/auth/user_manager.py
+++ server/auth/user_manager.py
@@ -51,7 +51,6 @@ class UserManager(IntegerIDMixin, BaseUserManager[User, int]):
         user_dict["is_superuser"] = False
         user_dict["is_active"] = True
         user_dict["is_verified"] = False
-        print(user_dict)
         created_user = await self.user_db.create(user_dict)
 
         await self.on_after_register(created_user, request)
