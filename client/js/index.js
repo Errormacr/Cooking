31ec30826c6d5cc13b23feeeb32474fb0ed0c62c
rakushka-san <vladislavmohov@mail.ru>
2023-03-08 19:50:06 +0300

    Создал файлы разметки и стилей для страницы рецепта

--- client/js/index.js
+++ client/js/index.js
@@ -13,17 +13,25 @@ $('document').ready(function() {
     //     
     // }
 
-    response = [{"recipe_id":1,"recipe_desc":{"name":"dssd","photo":"/photo/recipe/1_recipe_photo.jpg","servings_cout":1,"cook_time":"00:00:01","rating":0,"recommend":null,"author":1}}]
+    response = [{"recipe_id":3,"recipe_desc":{"name":"Котлеты морковные","photo":"/photo/recipe/3_recipe_photo.jpg","servings_cout":4,"cook_time":30,"rating":0,"recommend":"Берите свежую морковь","author":1}},{"recipe_id":4,"recipe_desc":{"name":"Борщ со сметаной","photo":"/photo/recipe/4_recipe_photo.jpg","servings_cout":3,"cook_time":280,"rating":0,"recommend":null,"author":1}}]
 
     var recipe_cards_container = $('#recipe-cards');
 
     var recipe_cards = [];
 
     response.forEach(recipe => {
+        var time = Number(recipe['recipe_desc']['cook_time']);
+        var hours = Math.floor(time / 60)
+
+        if (hours != 0) {
+            time = String(hours) + ' ч ' + String(time % 60 + ' мин');
+        } else {
+            time = String(time) + ' мин';
+        }
         recipe_cards.push(
             {
                 name: recipe['recipe_desc']['name'],
-                time: recipe['recipe_desc']['cook_time'],
+                time: time,
                 img_src: recipe['recipe_desc']['photo']
             }
         )
