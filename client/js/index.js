d07e97d0e953decbda546d2f2a40e211ae83079c
rakushka-san <vladislavmohov@mail.ru>
2023-03-09 23:07:21 +0300

    Сделал загрузку рецептов, косметические изменения

--- client/js/index.js
+++ client/js/index.js
@@ -1,41 +1,48 @@
-$('document').ready(function() {
-    // var server_url = 'https://4faf-178-141-127-143.eu.ngrok.io/';
+const server_url = 'https://31bb-178-141-127-143.eu.ngrok.io/';
 
-    // var recipes_request = new XMLHttpRequest();
+var offset = 0;
 
-    // recipes_request.open('GET', server_url + 'recipes/', true);
+async function fetch_recipes (offset) {
+    const response = await fetch(server_url + 'recipes/?limit=6&offset=' + offset, {
+        credentials: "include"
+    });
+    console.log(response);
+    const recipes = await response.json();
 
-    // recipes_request.send();
-
-    // console.log(recipes_request.responseText);
-
-    // if (recipes_request.status = 200) {
-    //     
-    // }
-
-    response = [{"recipe_id":3,"recipe_desc":{"name":"Котлеты морковные","photo":"/photo/recipe/3_recipe_photo.jpg","servings_cout":4,"cook_time":30,"rating":0,"recommend":"Берите свежую морковь","author":1}},{"recipe_id":4,"recipe_desc":{"name":"Борщ со сметаной","photo":"/photo/recipe/4_recipe_photo.jpg","servings_cout":3,"cook_time":280,"rating":0,"recommend":null,"author":1}}]
+    console.log(recipes);
 
     var recipe_cards_container = $('#recipe-cards');
 
     var recipe_cards = [];
 
-    response.forEach(recipe => {
+    recipes.forEach(recipe => {
         var time = Number(recipe['recipe_desc']['cook_time']);
-        var hours = Math.floor(time / 60)
+        var hours = Math.floor(time / 3600);
+        var minutes = time % 3600 / 60;
+        
+        time = (hours != 0 ? hours + ' ч' : "") + " " + (minutes != 0 ? minutes + ' мин' : "");
 
-        if (hours != 0) {
-            time = String(hours) + ' ч ' + String(time % 60 + ' мин');
-        } else {
-            time = String(time) + ' мин';
-        }
+        var img_src = server_url + 'recipes/photo/' + recipe['recipe_id'];
+
         recipe_cards.push(
             {
                 name: recipe['recipe_desc']['name'],
                 time: time,
-                img_src: recipe['recipe_desc']['photo']
+                img_src: img_src
             }
         )
     });
 
-    recipe_cards_container.loadTemplate('templates/recipe_card_tpl.html', recipe_cards);
+    recipe_cards_container.loadTemplate('templates/recipe_card_tpl.html', recipe_cards, {
+        append: true
+    });
+}
+
+function on_more_btn_click () {
+    fetch_recipes(offset);
+    offset += 6;
+}
+
+$('document').ready(function() {
+    $('#more_btn').click();
 });
