7b9d7a82525ff81c300f5b8fd3c4933fc13e5c5a
rakushka-san <vladislavmohov@mail.ru>
2023-03-27 00:55:57 +0300

    Всплывающие окна регистрации и авторизации

--- client/js/main.js
+++ client/js/main.js
@@ -18,10 +18,28 @@ function search_btn_click() {
     }
 };
 
+$.fn.popup = function() {
+    console.log($('.auth-modal').css('top'));
+    this.css('position', 'absolute').fadeIn();
+    this.css('top', ($(window).height() - this.height()) / 2 + $(window).scrollTop() - $('header').height() + 'px');
+    $('.modal-back').fadeIn();
+    $('body').css('overflow', 'hidden');
+}
+
 $('document').ready(function() { 
     var header_container = $('header');
     header_container.loadTemplate("templates/main/header_tpl.html", {});
 
     var footer_container = $('footer');
     footer_container.loadTemplate("templates/main/footer_tpl.html", {});
+
+
+    $('#profile_btn').click(function(){
+        $('.auth-modal').popup();
+    });
+    $('.modal-back').click(function(){
+        $('.auth-modal').fadeOut();
+        $('.modal-back').fadeOut();
+        $('body').css('overflow', 'auto');
+    });
 });
