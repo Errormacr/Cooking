a8645258575a50315f19c32a316198e08f2e6543
rakushka-san <vladislavmohov@mail.ru>
2023-03-27 01:17:59 +0300

    Навигация по окнам авторизации

--- client/js/main.js
+++ client/js/main.js
@@ -19,11 +19,47 @@ function search_btn_click() {
 };
 
 $.fn.popup = function() {
-    console.log($('.auth-modal').css('top'));
     this.css('position', 'absolute').fadeIn();
     this.css('top', ($(window).height() - this.height()) / 2 + $(window).scrollTop() - $('header').height() + 'px');
     $('.modal-back').fadeIn();
     $('body').css('overflow', 'hidden');
+}
+
+function on_profile_btn_click() {
+    if (sessionStorage.getItem('user_id')) {
+        const href = "profile.html?tab=1";
+        $(location).attr('href', href);
+    } else {
+        $('#sign_in_modal').popup();
+    }
+}
+
+function on_fav_btn_click() {
+    if (sessionStorage.getItem('user_id')) {
+        const href = "profile.html?tab=2";
+        $(location).attr('href', href);
+    } else {
+        $('#sign_in_modal').popup();
+    }
+}
+
+function on_add_recipe_btn_click() {
+    if (sessionStorage.getItem('user_id')) {
+        const href = "new_recipe.html";
+        $(location).attr('href', href);
+    } else {
+        $('#sign_in_modal').popup();
+    }
+}
+
+function on_sign_up_click() {
+    $('.auth-modal').fadeOut();
+    $('#sign_up_modal').popup();
+}
+
+function on_sign_in_click() {
+    $('.auth-modal').fadeOut();
+    $('#sign_in_modal').popup();
 }
 
 $('document').ready(function() { 
@@ -33,10 +69,14 @@ $('document').ready(function() {
     var footer_container = $('footer');
     footer_container.loadTemplate("templates/main/footer_tpl.html", {});
 
-
-    $('#profile_btn').click(function(){
-        $('.auth-modal').popup();
+    $('#modals_container').loadTemplate('templates/main/sign_in_modal_tpl.html', null, {
+        append: true
     });
+
+    $('#modals_container').loadTemplate('templates/main/sign_up_modal_tpl.html', null, {
+        append: true
+    });
+
     $('.modal-back').click(function(){
         $('.auth-modal').fadeOut();
         $('.modal-back').fadeOut();
