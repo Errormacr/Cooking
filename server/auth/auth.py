dcfcee535c018cdd553a01284b520ad14e6097c2
rakushka-san <vladislavmohov@mail.ru>
2023-03-27 20:34:54 +0300

    Настройка куки

--- server/auth/auth.py
+++ server/auth/auth.py
@@ -1,7 +1,7 @@
 from fastapi_users.authentication import CookieTransport, AuthenticationBackend, JWTStrategy
 from config import SECRET
 
-cookie_transport = CookieTransport(cookie_name="Cook", cookie_max_age=1209600, cookie_samesite=None)
+cookie_transport = CookieTransport(cookie_name="Cook", cookie_max_age=1209600, cookie_samesite="None")
 
 
 def get_jwt_strategy() -> JWTStrategy:
