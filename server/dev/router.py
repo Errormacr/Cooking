b323f1d9069a90dc37eb136b56d2cd9aa2459d89
Errormacr <tulupov.valeriy@mail.ru>
2023-03-28 17:06:11 +0300

    Удаление не всего

--- server/dev/router.py
+++ server/dev/router.py
@@ -11,12 +11,13 @@ from typing import List
 from dev.shemas import Ingredient_create
 from models import Tag, Ingredient, Unit
 from config import KEY
+
 router = APIRouter(prefix="/dev", tags=["dev"])
 
 current_user = fastapi_users.current_user()
 
 
-@router.post("/ingredient/", status_code=201,tags=["ingredient"])
+@router.post("/ingredient/", status_code=201, tags=["ingredient"])
 async def create_ingredient(ingredient: Ingredient_create, key: str,
                             session: AsyncSession = Depends(get_async_session)):
     if key != KEY:
@@ -31,7 +32,7 @@ async def create_ingredient(ingredient: Ingredient_create, key: str,
     return ingredient
 
 
-@router.post("/unit/", status_code=201,tags=["unit"])
+@router.post("/unit/", status_code=201, tags=["unit"])
 async def create_unit(unit: str, key: str, session: AsyncSession = Depends(get_async_session)):
     if key != KEY:
         raise HTTPException(status_code=400, detail="Wrong key")
@@ -46,29 +47,23 @@ async def create_unit(unit: str, key: str, session: AsyncSession = Depends(get_a
     return {"unit": unit}
 
 
-
-
-
-
-
-
-@router.delete("/ingredient/", status_code=204,tags=["ingredient"])
+@router.delete("/ingredient/", status_code=204, tags=["ingredient"])
 async def delete_ingredient(key: str, ingredient_id: int, session: AsyncSession = Depends(get_async_session)):
     if key != KEY:
         raise HTTPException(status_code=400, detail={"Error": "Wrong key"})
     try:
-        await session.execute( delete(Ingredient).where(Ingredient.c.tag_ID == ingredient_id))
+        await session.execute(delete(Ingredient).where(Ingredient.c.tag_ID == ingredient_id))
         await session.commit()
     except exc.DataError:
         return {"Error": "Data error"}
 
 
-@router.delete("/unit/", status_code=204,tags=["unit"])
+@router.delete("/unit/", status_code=204, tags=["unit"])
 async def delete_unit(key: str, unit_id: int, session: AsyncSession = Depends(get_async_session)):
     if key != KEY:
         raise HTTPException(status_code=400, detail={"Error": "Wrong key"})
     try:
-        await session.execute( delete(Unit).where(Unit.c.unit_ID == unit_id))
+        await session.execute(delete(Unit).where(Unit.c.unit_ID == unit_id))
         await session.commit()
     except exc.DataError:
         return {"Error": "Data error"}
